3c8d0536f2922d518823f9dd10886e0327505b48
abram-stack <anthonybramantyo@outlook.com>
2022-12-22 08:19:59 +0100

    fix highlight, add function switching tabs

--- tabs.js
+++ tabs.js
@@ -1,5 +1,6 @@
 // to make the keyboard tab for accessive assitance
 
+// TAB
 // we can select the class or
 // we reinforce, make sure thing accessible, person use the role
 const tabList = document.querySelector('[role="tablist"]');
@@ -11,7 +12,9 @@ tabs.forEach((tab) => {
   tab.addEventListener('click', changeTabPanel);
 });
 
+
 let tabFocus = 0;
+
 function changeTabFocus(e) {
   const keydownLeft = 37;
   const keydownRight = 39;
@@ -20,25 +23,25 @@ function changeTabFocus(e) {
   // change the tabindex of the current tab to -1
   if (e.keyCode === keydownLeft || e.keyCode === keydownRight) {
     tabs[tabFocus].setAttribute('tabindex', -1);
-  }
-  // if the right key is pushed, move to the next tab on the right
-  if (e.keyCode === keydownRight) {
-    tabFocus++;
-    if (tabFocus >= tabs.length) {
-      tabFocus = 0;
+    // if the right key is pushed, move to the next tab on the right
+    if (e.keyCode === keydownRight) {
+      tabFocus++;
+      if (tabFocus >= tabs.length) {
+        tabFocus = 0;
+      }
     }
-  }
-  // if the left key is pushed, move to the next tab on the left
-  if (e.keyCode === keydownLeft) {
-    tabFocus--;
-    if (tabFocus < 0) {
-      tabFocus = tabs.length - 1;
+    // if the left key is pushed, move to the next tab on the left
+    else if(e.keyCode === keydownLeft) {
+      tabFocus--;
+      if (tabFocus < 0) {
+        tabFocus = tabs.length - 1;
+      }
     }
-  }
 
-  // set the current tab to be 0, next tab can be focus on, but not focus immediately
-  tabs[tabFocus].setAttribute('tabindex', 0);
-  tabs[tabFocus].focus();
+    // set the current tab to be 0, next tab can be focus on, but not focus immediately
+    tabs[tabFocus].setAttribute('tabindex', 0);
+    tabs[tabFocus].focus();
+  }
 }
 
 function changeTabPanel(e) {
@@ -49,24 +52,30 @@ function changeTabPanel(e) {
   const tabContainer = targetTab.parentNode;
   const mainContainer = tabContainer.parentNode;
 
-
   // GOAL: not to make each article on top of each other.
-  // make this disappear first. we find article element, set hidden to true, 
-  mainContainer
-    .querySelectorAll('[role="tabpanel"]')
-    .forEach((panel) => panel.setAttribute('hidden', true));
+  // make this disappear first. we find article element, set hidden to true,
   // find the selected tab(find the id),  to make it appears
-  mainContainer.querySelector([`#${targetPanel}`]).removeAttribute('hidden');
-  
-  // GOAL: not to make each picture on top of each other.
-  // make the picture disappear first. in our html, we created extra id to picture, and extra attribute to our tab
-  mainContainer
-    .querySelectorAll('picture')
-    .forEach((picture) => picture.setAttribute('hidden', true));
-  // make it appears when selected tab is targeted
-  mainContainer.querySelector([`#${targetImage}`]).removeAttribute('hidden');
+  hideContent(mainContainer, '[role = "tabpanel"]');
+  showContent(mainContainer, [`#${targetPanel}`]);
 
-   // if we change tabs, we remove the highlight and selected
-  tabContainer.querySelector('[aria-selected="true"]').setAttribute('aria-selected', false);
+  // in our html, we created extra id to picture, and extra attribute to our tab
+  hideContent(mainContainer, 'picture');
+  showContent(mainContainer, [`#${targetImage}`]);
+
+  // highlight of selected tab
+  // if we change tabs, we remove the highlight and selected
+  tabContainer
+    .querySelector('[aria-selected="true"]')
+    .setAttribute('aria-selected', false);
   targetTab.setAttribute('aria-selected', true);
+}
+
+function hideContent(parent, content) {
+  parent
+    .querySelectorAll(content)
+    .forEach((item) => item.setAttribute('hidden', true));
+}
+
+function showContent(parent, content) {
+  parent.querySelector(content).removeAttribute('hidden');
 }
